c8c17f193441ddad7ae06bf1d9539a0ffb12755e
Edd Mann <the@eddmann.com>
2020-12-11 17:11:33 +0000

    Add 2020 day 11

---
+++ 2020/day11/solution.clj
@@ -0,0 +1,52 @@
+(load-file "/src/helpers.clj")
+
+(defn parse-grid [filename]
+  (mapv #(clojure.string/split % #"") (read-lines filename)))
+
+(defn next-seat-occupied? [grid row col row-offset col-offset multipler]
+  (case (get-in grid [(+ row (* row-offset multipler)) (+ col (* col-offset multipler))])
+    "#" true
+    "L" false
+    nil false
+    (recur grid row col row-offset col-offset (inc multipler))))
+
+(defn count-occupied-seats [grid row col check-next-seat?]
+  (reduce
+    (fn [acc [row-offset col-offset]]
+      (let [seat (get-in grid [(+ row row-offset) (+ col col-offset)])]
+        (cond
+          (= seat "#") (inc acc)
+          (and (= seat ".") check-next-seat? (next-seat-occupied? grid row col row-offset col-offset 2)) (inc acc)
+          :else acc)))
+    0
+    (for [row-offset [-1 0 1] col-offset [-1 0 1] :when (not= [row-offset col-offset] [0 0])]
+      [row-offset col-offset])))
+
+(defn gen-next-grid [grid min-occupied-seats check-next-seat?]
+  (reduce
+    (fn [next-grid [row col]]
+      (let [occupied-seats (count-occupied-seats grid row col check-next-seat?)
+            seat (get-in grid [row col])]
+        (cond
+          (and (= seat "L") (zero? occupied-seats)) (assoc-in next-grid [row col] "#")
+          (and (= seat "#") (> occupied-seats min-occupied-seats)) (assoc-in next-grid [row col] "L")
+          :else next-grid)))
+    grid
+    (for [row (range 0 (count grid)) col (range 0 (count (first grid)))]
+      [row col])))
+
+(defn part1 [grid]
+  (let [next-grid (gen-next-grid grid 3 false)]
+    (if (= grid next-grid)
+      (get (frequencies (flatten next-grid)) "#")
+      (recur next-grid))))
+
+(defn part2 [grid]
+  (let [next-grid (gen-next-grid grid 4 true)]
+    (if (= grid next-grid)
+      (get (frequencies (flatten next-grid)) "#")
+      (recur next-grid))))
+
+(def grid (parse-grid "/src/day11/input.txt"))
+(println (part1 grid))
+(println (part2 grid))
---
+++ 2020/day11/solution.js
@@ -0,0 +1,144 @@
+const fs = require("fs");
+
+const isEqualGrid = (gridA, gridB) => {
+  for (let r = 0; r < gridA.length; r++) {
+    for (let c = 0; c < gridA[0].length; c++) {
+      if (gridA[r][c] !== gridB[r][c]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+const cloneGrid = (grid) => grid.map((cols) => [...cols]);
+
+const getSeatValue = (grid, row, col, rowOffset, colOffset) => {
+  let nextRow = row + rowOffset;
+  let nextCol = col + colOffset;
+
+  if (
+    nextRow < 0 ||
+    nextRow >= grid.length ||
+    nextCol < 0 ||
+    nextCol >= grid[0].length
+  ) {
+    return undefined;
+  }
+
+  return grid[nextRow][nextCol];
+};
+
+const isNextSeatOccupied = (
+  grid,
+  row,
+  col,
+  rowOffset,
+  colOffset,
+  multipler
+) => {
+  const seat = getSeatValue(
+    grid,
+    row,
+    col,
+    rowOffset * multipler,
+    colOffset * multipler
+  );
+
+  if (seat === undefined || seat === "L") {
+    return false;
+  }
+
+  if (seat === "#") {
+    return true;
+  }
+
+  return isNextSeatOccupied(
+    grid,
+    row,
+    col,
+    rowOffset,
+    colOffset,
+    multipler + 1
+  );
+};
+
+const countOccupiedSeats = (grid, row, col, isCheckNextSeat) => {
+  let count = 0;
+
+  for (let rowOffset = -1; rowOffset <= 1; rowOffset++) {
+    for (let colOffset = -1; colOffset <= 1; colOffset++) {
+      if (rowOffset === 0 && colOffset === 0) continue;
+
+      let seat = getSeatValue(grid, row, col, rowOffset, colOffset);
+
+      if (seat === "#") {
+        count++;
+      }
+
+      if (
+        isCheckNextSeat &&
+        seat === "." &&
+        isNextSeatOccupied(grid, row, col, rowOffset, colOffset, 2)
+      ) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+};
+
+const generateNextGrid = (grid, minOccupiedSeats, isCheckNextSeat) => {
+  const outputGrid = cloneGrid(grid);
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      let occupiedSeats = countOccupiedSeats(grid, row, col, isCheckNextSeat);
+      let seat = grid[row][col];
+
+      if (seat === "L" && occupiedSeats === 0) {
+        outputGrid[row][col] = "#";
+      } else if (seat === "#" && occupiedSeats > minOccupiedSeats) {
+        outputGrid[row][col] = "L";
+      }
+    }
+  }
+
+  return outputGrid;
+};
+
+const part1 = (grid) => {
+  let prev = grid;
+  let next;
+
+  while (true) {
+    next = generateNextGrid(prev, 3, false);
+    if (isEqualGrid(next, prev)) break;
+    prev = next;
+  }
+
+  return prev.flat().filter((seat) => seat === "#").length;
+};
+
+const part2 = (grid) => {
+  let prev = grid;
+  let next;
+
+  while (true) {
+    next = generateNextGrid(prev, 4, true);
+    if (isEqualGrid(next, prev)) break;
+    prev = next;
+  }
+
+  return prev.flat().filter((seat) => seat === "#").length;
+};
+
+const grid = fs
+  .readFileSync("./input.txt", "utf-8")
+  .split("\n")
+  .map((x) => x.split(""));
+
+console.log(part1(grid));
+console.log(part2(grid));
