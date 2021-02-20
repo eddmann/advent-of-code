e1e7a9db877ef7638c5be5a151598cd865995337
Edd Mann <the@eddmann.com>
2021-02-20 15:50:54 +0000

    Conditional execute solution part if present

--- 2020/typescript/src/solve.ts
+++ 2020/typescript/src/solve.ts
@@ -16,8 +16,12 @@ const daySrcDir = `${__dirname}/day${dayNo.toString().padStart(2, '0')}`;
 const { part1, part2 } = require(`${daySrcDir}/solution-${solutionNo}`);
 const input = readFileSync(`${daySrcDir}/input.txt`, 'utf-8').trim();
 
-const p1 = exec(part1, input);
-console.log(`Part 1: ${p1.result} [time taken: ${p1.duration} ms]`);
+if (part1) {
+  const p1 = exec(part1, input);
+  console.log(`Part 1: ${p1.result} [time taken: ${p1.duration} ms]`);
+}
 
-const p2 = exec(part2, input);
-console.log(`Part 2: ${p2.result} [time taken: ${p2.duration} ms]`);
+if (part2) {
+  const p2 = exec(part2, input);
+  console.log(`Part 2: ${p2.result} [time taken: ${p2.duration} ms]`);
+}
