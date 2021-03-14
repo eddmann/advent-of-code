55f1edb9cbadc65b8cf56a5baa2321b19bb60caa
Edd Mann <the@eddmann.com>
2021-03-14 17:46:09 +0000

    Add AoC 2016 Python Day 10

---
+++ 2016/python/src/day10/solution-1.py
@@ -0,0 +1,52 @@
+import collections
+import re
+import math
+
+
+def parse_values(instructions):
+    return [val.groups() for line in instructions if (
+        val := re.match(r'value (\d+) goes to (bot \d+)', line))]
+
+
+def parse_allocations(instructions):
+    return [alloc.groups() for line in instructions if (
+        alloc := re.match(r'(bot \d+) gives low to (\w+ \d+) and high to (\w+ \d+)', line))]
+
+
+def setup_bins(allocations):
+    bins = collections.defaultdict(lambda: lambda x: x)
+
+    def config_bot(low_recipient, high_recipient):
+        def awaiting_first_chip(a):
+            def awaiting_second_chip(b):
+                l, h = sorted((a, b))
+                bins[low_recipient] = bins[low_recipient](l)
+                bins[high_recipient] = bins[high_recipient](h)
+                return (l, h)
+            return awaiting_second_chip
+        return awaiting_first_chip
+
+    for bot, low, high in allocations:
+        bins[bot] = config_bot(low, high)
+
+    return bins
+
+
+def part1(input):
+    instructions = input.splitlines()
+    bins = setup_bins(parse_allocations(instructions))
+
+    for val, bot in parse_values(instructions):
+        bins[bot] = bins[bot](int(val))
+
+    return next(bot for bot, allocated in bins.items() if allocated == (17, 61))
+
+
+def part2(input):
+    instructions = input.splitlines()
+    bins = setup_bins(parse_allocations(instructions))
+
+    for val, bot in parse_values(instructions):
+        bins[bot] = bins[bot](int(val))
+
+    return math.prod(bins['output {}'.format(i)] for i in range(3))
