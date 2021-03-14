e8cc0dd02dad2ebd179b3eec25c17a14929ee7dc
Edd Mann <the@eddmann.com>
2021-03-14 17:45:51 +0000

    Add AoC 2016 Python Day 7

---
+++ 2016/python/src/day07/solution-1.py
@@ -0,0 +1,30 @@
+import re
+
+
+def parse_ips(input):
+    def parse_ip(ip):
+        supernets = re.split(r"\[[^\]]+\]", ip)
+        hypernets = re.findall(r"\[([^\]]+)\]", ip)
+        return supernets, hypernets
+    return map(parse_ip, input.splitlines())
+
+
+def has_tls_support(ip):
+    supernets, hypernets = ip
+    abba = r"(.)((?!\1).)\2\1"
+    return any(re.search(abba, s) for s in supernets) and not any(re.search(abba, h) for h in hypernets)
+
+
+def has_ssl_support(ip):
+    supernets, hypernets = ip
+    aba = iter(ab for s in supernets for ab in re.findall(
+        r"(?=(.)((?!\1).)\1)", s))
+    return any(b + a + b in h for a, b in aba for h in hypernets)
+
+
+def part1(input):
+    return sum(has_tls_support(ip) for ip in parse_ips(input))
+
+
+def part2(input):
+    return sum(has_ssl_support(ip) for ip in parse_ips(input))
