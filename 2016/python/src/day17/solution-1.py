8d055600f5a078d528e82387fcee5c35b4d415c1
Edd Mann <the@eddmann.com>
2021-03-18 08:55:55 +0000

    Add AoC 2016 Python Day 17

---
+++ 2016/python/src/day17/solution-1.py
@@ -0,0 +1,44 @@
+import collections
+import itertools
+import hashlib
+
+
+def md5(value):
+    return hashlib.md5(value.encode('utf-8')).hexdigest()
+
+
+def open_doors(passcode, directions):
+    return itertools.compress(
+        [('U', (0, -1)), ('D', (0, 1)), ('L', (-1, 0)), ('R', (1, 0))],
+        (int(d, 16) > 10 for d in md5(passcode + directions)[:4])
+    )
+
+
+def vault_access_routes(passcode, start, vault):
+    queue = collections.deque([(start, '')])
+
+    while queue:
+        (x, y), directions = queue.popleft()
+
+        for direction, (dx, dy) in open_doors(passcode, directions):
+            nx, ny = x + dx, y + dy
+
+            if not (0 <= nx < 4 and 0 <= ny < 4):
+                continue
+
+            nposition = (nx, ny)
+            ndirections = directions + direction
+
+            if (nposition == vault):
+                yield ndirections
+                continue
+
+            queue.append((nposition, ndirections))
+
+
+def part1(input):
+    return next(vault_access_routes(input, (0, 0), (3, 3)))
+
+
+def part2(input):
+    return len(list(vault_access_routes(input, (0, 0), (3, 3)))[-1])
