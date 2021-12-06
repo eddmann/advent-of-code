57252ef00377578243110e32548f5eab59490bf9
Edd Mann <the@eddmann.com>
2021-12-06 23:13:38 +0000

    Add example C++ AoC wrapper exec

---
+++ 2021/c/aoc2021.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include "day01/input.h"
+#include "day01/solution.c"
+#include "day02/input.h"
+#include "day02/solution.c"
+#include "day03/input.h"
+#include "day03/solution.c"
+#include "day04/input.h"
+#include "day04/solution.c"
+#include "day05/input.h"
+#include "day05/solution.c"
+#include "day06/input.h"
+#include "day06/solution.c"
+
+#define PRINT_DAY(day) \
+  std::cout << #day << "\n"; \
+  std::cout << day##_part1(day##_input) << "\n"; \
+  std::cout << day##_part2(day##_input) << "\n\n";
+
+int main() {
+  PRINT_DAY(day01)
+  PRINT_DAY(day02)
+  PRINT_DAY(day03)
+  PRINT_DAY(day04)
+  PRINT_DAY(day05)
+  PRINT_DAY(day06)
+
+  return 0;
+}
