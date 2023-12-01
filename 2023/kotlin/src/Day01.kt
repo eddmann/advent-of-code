7691dd1d69410e1a3a90578e03a79a5560a83ee6
Edd Mann <the@eddmann.com>
2023-12-01 14:23:43 +0000

    Add 2023/01

--- 2023/kotlin/src/Day01.kt
+++ 2023/kotlin/src/Day01.kt
@@ -1,6 +1,31 @@
-private fun part1(input: String) = 0
+private fun calibrate(values: List<String>) = values.sumOf { value ->
+    val digits = value.replace(Regex("\\D"), "")
+    "${digits.first()}${digits.last()}".toInt()
+}
 
-private fun part2(input: String) = 0
+private fun part1(input: String): Int = calibrate(input.lines())
+
+private fun part2(input: String): Int {
+    val translations = mapOf(
+        "one" to "o1e",
+        "two" to "t2o",
+        "three" to "t3e",
+        "four" to "4",
+        "five" to "5e",
+        "six" to "6",
+        "seven" to "7n",
+        "eight" to "e8t",
+        "nine" to "n9e",
+    )
+
+    val values = input.lines().map { value ->
+        translations.entries.fold(value) { acc, (from, to) ->
+            acc.replace(from, to)
+        }
+    }
+
+    return calibrate(values)
+}
 
 fun main() {
     val input = readInput("Day01")
