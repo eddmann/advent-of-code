d0922a26042f7f93092974e0a2f08d46444e01f1
Edd Mann <the@eddmann.com>
2023-12-09 13:46:35 +0000

    Add 2023/08

---
+++ 2023/kotlin/src/Day08.kt
@@ -0,0 +1,60 @@
+enum class Instruction { LEFT, RIGHT }
+
+typealias Position = String
+typealias Mapping = Map<Position, Direction>
+
+data class Direction(val left: Position, val right: Position)
+
+private fun parseInstructionsAndMap(input: String): Pair<List<Instruction>, Mapping> {
+    val parts = input.split("\n\n")
+
+    val instructions = parts[0]
+        .map { if (it == 'L') { Instruction.LEFT } else { Instruction.RIGHT } }
+
+    val map = parts[1]
+        .lines()
+        .associate { line ->
+            val (position, left, right) = Regex("([A-Z0-9]{3}).*([A-Z0-9]{3}).*([A-Z0-9]{3})").find(line)!!.destructured
+            Pair(position, Direction(left, right))
+        }
+
+    return Pair(instructions, map)
+}
+
+private fun countSteps(instructions: List<Instruction>, map: Mapping, initial: Position): Int {
+    var position = initial
+
+    instructions.cycle().forEachIndexed { steps, instruction ->
+        position = if (instruction == Instruction.LEFT) { map[position]!!.left } else { map[position]!!.right }
+
+        if (position.endsWith("Z")) {
+            return steps + 1
+        }
+    }
+
+    return 0
+}
+
+private fun part1(input: String): Int {
+    val (instructions, map) = parseInstructionsAndMap(input)
+    return countSteps(instructions, map, "AAA")
+}
+
+private fun part2(input: String): Long {
+    fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
+    fun lcm(a: Long, b: Long): Long = a / gcd(a, b) * b
+
+    val (instructions, map) = parseInstructionsAndMap(input)
+
+    return map
+        .keys
+        .filter { it.endsWith("A") }
+        .map { countSteps(instructions, map, it).toLong() }
+        .reduce(::lcm)
+}
+
+fun main() {
+    val input = readInput("Day08")
+    part1(input).println()
+    part2(input).println()
+}
