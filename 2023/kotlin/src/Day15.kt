225530acc4ed382d4089f0c754c1e1b85915414a
Edd Mann <the@eddmann.com>
2023-12-20 09:31:34 +0000

    Add 2023/15 in Kotlin/Swift

---
+++ 2023/kotlin/src/Day15.kt
@@ -0,0 +1,43 @@
+typealias Lens = Pair<String, Int>
+
+private fun hash(step: String) =
+    step.fold(0) { acc, chr -> ((acc + chr.code) * 17) % 256 }
+
+private fun part1(input: String) =
+    input.split(",").sumOf(::hash)
+
+private fun part2(input: String): Int {
+    val boxes = mutableMapOf<Int, MutableList<Lens>>().withDefault { mutableListOf() }
+    val lenses = input.split(",")
+
+    for (lens in lenses) {
+        val label = lens.takeWhile { it != '=' && it != '-' }
+
+        val box = boxes.getValue(hash(label))
+        boxes[hash(label)] = box
+
+        if (lens.endsWith("-")) {
+            box.removeAll { it.first == label }
+            continue
+        }
+
+        val value = lens.takeLast(1).toInt()
+        val existing = box.indexOfFirst { it.first == label }
+
+        if (existing == -1) {
+            box.add(Pair(label, value))
+        } else {
+            box[existing] = Pair(label, value)
+        }
+    }
+
+    return boxes.toList().sumOf { (boxNo, box) ->
+        box.mapIndexed { slotNo , (_, value) -> (boxNo + 1) * (slotNo + 1) * value }.sum()
+    }
+}
+
+fun main() {
+    val input = readInput("Day15")
+    part1(input).println()
+    part2(input).println()
+}
